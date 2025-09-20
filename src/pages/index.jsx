60e7c1079285f1770de005ce8650d490c6af9ab1
zanyarahmed11 <zanyarahmed11@gmail.com>
2025-09-20 15:53:01 +0000

    feat: set home-digital-agency as main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,49 +1,45 @@
-import { useEffect } from 'react';
 import Head from 'next/head';
 //= Layout
-import PreviewLayout from '@layouts/Preview';
+import MainLayout from '@layouts/Main';
 //= Components
-import Header from '@components/Preview/Header';
-import Features from '@components/Preview/Features';
-import Demos from '@components/Preview/Demos';
-import InnerPages from '@components/Preview/InnerPages';
-import BuyNow from '@components/Preview/BuyNow';
-import Portfolio from '@components/Preview/Portfolio';
-import Codei from '@components/Preview/Codei';
-import BestFeatures from '@components/Preview/BestFeatures';
-import Responsive from '@components/Preview/Responsive';
-import AllFeatures from '@components/Preview/AllFeatures';
-import Testimonials from '@components/Preview/Testimonials';
-import CallToAction from '@components/Preview/CallToAction';
+import TopNav from '@components/Navbars/DigitalNav/TopNav';
+import Navbar from '@components/Navbars/DigitalNav';
+import Header from '@components/Digital/Header';
+import About from '@components/Digital/About';
+import Services from '@components/Digital/Services';
+import ChooseUs from '@components/Digital/ChooseUs';
+import Portfolio from '@components/Digital/Portfolio';
+import Testimonials from '@components/Digital/Testimonials';
+import Team from '@components/Digital/Team';
+import Blog from '@components/Digital/Blog';
+import Contact from '@components/Digital/Contact';
+import Footer from '@components/Digital/Footer';
 
-const LandingPreview = () => {
-  useEffect(() => {
-    document.body.classList.add('index-main');
-    return () => document.body.classList.remove('index-main');
-  }, []);
-
+const HomeDigitalAgency = () => {
   return (
     <>
       <Head>
-        <title>Iteck - Preview</title>
+        <title>Iteck - Digital Agency</title>
       </Head>
 
-      <PreviewLayout>
+      <MainLayout scrollTopText>
+        <TopNav />
+        <Navbar />
         <Header />
-        <Features />
-        <Demos />
-        <InnerPages />
-        <BuyNow />
-        <Portfolio />
-        <Codei />
-        <BestFeatures />
-        <Responsive />
-        <AllFeatures />
-        <Testimonials />
-        <CallToAction />
-      </PreviewLayout>
+        <main>
+          <About />
+          <Services />
+          <ChooseUs />
+          <Portfolio />
+          <Testimonials />
+          <Team />
+          <Blog />
+          <Contact />
+        </main>
+        <Footer />
+      </MainLayout>
     </>
   )
 }
 
-export default LandingPreview;
+export default HomeDigitalAgency;
